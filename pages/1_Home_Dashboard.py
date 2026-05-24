242f688f3eddd184ff5de2a623117a4478c10ab4
alpa9875 <alicia.palm1992@gmail.com>
2026-05-24 17:01:16 -0600

    Update 1_Home_Dashboard.py

--- pages/1_Home_Dashboard.py
+++ pages/1_Home_Dashboard.py
@@ -13,7 +13,7 @@ def load_inventory_summary():
         SELECT
             (SELECT COUNT(*) FROM personal_set_inv) AS total_sets,
             (SELECT SUM(retail_price * quantity) FROM personal_set_inv) AS total_retail_price,
-            (SELECT SUM(retail_value * quantity) FROM personal_set_inv) AS total_retail_value,
+            (SELECT SUM(retail_value * quantity) FROM personal_set_inv) AS total_aftermarket_value,
             (SELECT SUM(grand_total_qty) FROM set_parts_inventory) AS total_parts
     """
     return pd.read_sql(query, engine)
@@ -36,7 +36,7 @@ summary = load_inventory_summary()
 col1, col2, col3, col4 = st.columns(4)
 col1.metric("Total Sets", int(summary["total_sets"][0]))
 col2.metric("Total Retail Price", f"${summary['total_retail_price'][0]:,.0f}")
-col3.metric("Total Aftermarket Value", f"${summary['total_retail_value'][0]:,.0f}")
+col3.metric("Total Aftermarket Value", f"${summary['total_aftermarket_value'][0]:,.0f}")
 col4.metric("Total Parts", f"{summary['total_parts'][0]:,}")
 
 st.subheader("Top Themes in My Collection")
