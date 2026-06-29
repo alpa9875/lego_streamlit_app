24ca11ebe404e876b39849a6cb3245dca273e391
alpa9875 <alicia.palm1992@gmail.com>
2026-06-29 01:57:50 +0000

    Fixed photo rotations

--- pages/1_Home_Dashboard.py
+++ pages/1_Home_Dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from utils.db import get_engine
 import plotly.express as px
-from PIL import Image
+from PIL import Image, ImageOps
 
 st.title("Home Dashboard")
 
@@ -41,17 +41,17 @@ col2.metric("Total Retail Price", f"${summary['total_retail_price'][0]:,.0f}")
 col3.metric("Total Aftermarket Value", f"${summary['total_aftermarket_value'][0]:,.0f}")
 col4.metric("Total Parts", f"{summary['total_parts'][0]:,}")
 
-image1 = Image.open("images/20260628_181631.jpg")
+image1 = ImageOps.exif_transpose(Image.open("images/20260628_181631.jpg"))
 st.image(image1, caption = "Displayed Sets on Bookcase", use_column_width=True)
 
-image2 = Image.open("images/20260628_181724.jpg")
+image2 = ImageOps.exif_transpose(Image.open("images/20260628_181724.jpg"))
 st.image(image2, caption = "Displayed Sets on High Shelf", use_column_width=True)
 
-image3 = Image.open("images/20260628_181905.jpg")
+image3 = ImageOps.exif_transpose(Image.open("images/20260628_181905.jpg"))
 st.image(image3, caption = "Displayed Sets on Wire Shelf", use_column_width=True)
 
-image4 = Image.open("images/20260628_181951.jpg")
+image4 = ImageOps.exif_transpose(Image.open("images/20260628_181951.jpg"))
 st.image(image4, caption="Lego Room Angle 1", use_column_width=True)
 
-image5 = Image.open("images/20260628_182019.jpg")
+image5 = ImageOps.exif_transpose(Image.open("images/20260628_182019.jpg"))
 st.image(image5, caption="Lego Room Angle 2", use_column_width=True)
